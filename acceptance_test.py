eace2fec6a08458fb4d865bd3b0169b56677f5b4
jsrois <javier.sanchez.rois@gmail.com>
2017-07-07 19:06:05 +0200

    Acceptance test

---
+++ acceptance_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestAcceptance(unittest.TestCase):
+    def test_updates_parameter_values_from_command_line_arguments(self):
+        parser = argparser.ArgParser(schema={"-l", ("-p", 8000), ("-b", "/usr/log")})
+        parser.parse("-l -p 8080 -b /usr/local/log")
+        self.assertEqual(parser.get("-l"), True)
+        self.assertEqual(parser.get("-p"), 8080)
+        self.assertEqual(parser.get("-b"), "/usr/local/log")
+
+
+if __name__ == "__main__":
+    unittest.main()
